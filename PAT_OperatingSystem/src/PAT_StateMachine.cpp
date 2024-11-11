ef3243dd9e25ca8efebefd5e52e43c8047d78c9e
PAT-IOT <pourya.iot@Gmail.com>
2024-11-11 22:53:45 +0330

    changed state machin

--- PAT_OperatingSystem/src/PAT_StateMachine.cpp
+++ PAT_OperatingSystem/src/PAT_StateMachine.cpp
@@ -1,49 +1,38 @@
 #include "PAT_StateMachine.h"
 #include "PAT_Debug.h"
 
-StateMachine::StateMachine() : currentState(""), errorHandler(nullptr),
-                               Q_1_State(""), Q_2_State(""), Q_3_State(""),
-                               Q_4_State(""), Q_5_State(""), Q_6_State(""), suspended(false), stateMachineName(""), force_suspend(false), logon(false), Class_Log(COLOR_LIGHT_BLUE, TEXT_BOLD, "[sm]:")
+StateMachine::StateMachine() : errorHandler(nullptr), enterHandler(nullptr), exitHandler(nullptr),
+                               suspended(false), force_suspend(false), logon(false),
+                               Class_Log(COLOR_LIGHT_BLUE, TEXT_BOLD, "[sm]:")
 {
-    for (int i = 0; i < _STATE_BUFFER; i++)
+    for (int i = 0; i < stateQueue * 2 + 1; i++)
     {
-        nextState[i] = "";
+        Q[i] = "";
     }
+    StartState("setIdleState", []() {});
 }
-// Destructor: Delete all created queues
-// StateMachine::~StateMachine()
-// {
-//     for (auto &queuePair : sendQueues)
-//     {
-//         if (queuePair.second != nullptr)
-//         {
-//             vQueueDelete(queuePair.second);
-//         }
-//     }
-//     for (auto &queuePair : receiveQueues)
-//     {
-//         if (queuePair.second != nullptr)
-//         {
-//             vQueueDelete(queuePair.second);
-//         }
-//     }
-// }
-// Create a queue if it doesn't exist for the specified key
-
 void StateMachine::StartState(const String &stateName, StateFunction stateFunc)
 {
     states[stateName] = stateFunc;
-    if (currentState == "")
+    if (Q[stateQueue] == "")
     {
-        currentState = stateName; // Set the initial state
+        Q[stateQueue] = stateName; // Set the initial state
+    }
+    else
+    {
+        setNextState(stateName, 0);
     }
 }
 void StateMachine::StartState(const String &stateName)
 {
 
-    if (currentState == "")
+    if (Q[stateQueue] == "")
     {
-        currentState = stateName; // Set the initial state
+        Q[stateQueue] = stateName; // Set the initial state
+    }
+    else
+    {
+        setNextState(stateName, 0);
     }
 }
 void StateMachine::addState(const String &stateName, StateFunction stateFunc)
@@ -51,40 +40,49 @@ void StateMachine::addState(const String &stateName, StateFunction stateFunc)
     states[stateName] = stateFunc; // Add or replace state function
 }
 
-void StateMachine::onEnterState(StateTransitionHandler handler)
+void StateMachine::onEnterState(eventFunction handler)
 {
     enterHandler = handler;
 }
 
-void StateMachine::onExitState(StateTransitionHandler handler)
+void StateMachine::onExitState(eventFunction handler)
 {
     exitHandler = handler;
 }
 
-void StateMachine::setErrorHandler(ErrorHandler handler)
+void StateMachine::setErrorHandler(eventFunction handler)
 {
     errorHandler = handler;
 }
-void StateMachine::logOn(String statemachinename)
+void StateMachine::logOff(void)
 {
+    logon = false;
+    enterHandler = nullptr;
+    exitHandler = nullptr;
+    errorHandler = nullptr;
+}
+
+void StateMachine::logOn(String stateMachineName)
+{
+    init(COLOR_LIGHT_BLUE, TEXT_BOLD, "[sm]:[%s]: ", stateMachineName.c_str());
     logon = true;
-    stateMachineName = statemachinename;
+
     onEnterState([&](const String &stateName)
                  {
                      //------------------------------
-                     log(COLOR_LIGHT_GREEN, TEXT_NORMAL, "[%s]: Entering state: %s\n", stateMachineName.c_str(), stateName.c_str());
+                     log(COLOR_LIGHT_GREEN, TEXT_NORMAL, "Entering state: %s\n", stateName.c_str());
                      //------------------------------
                  });
     onExitState([&](const String &stateName)
                 {
                     //------------------------------
-                    log(COLOR_TEAL, TEXT_NORMAL, "[%s]: Exiting state: %s\n", stateMachineName.c_str(), stateName.c_str());
+                    log(COLOR_TEAL, TEXT_NORMAL, "Exiting state: %s\n", stateName.c_str());
                     //------------------------------
                 });
     setErrorHandler([&](const String &errorMessage)
                     {
                         //------------------------------
-                        log(COLOR_RED, TEXT_NORMAL, "[%s]: Error state not found:  %s\n", stateMachineName.c_str(), errorMessage.c_str());
+                        log(COLOR_RED, TEXT_NORMAL, "Error state not found:  %s\n", errorMessage.c_str());
                         //------------------------------
                     });
 }
@@ -92,32 +90,34 @@ void StateMachine::update()
 {
     if (!force_suspend)
     {
+        //----------------------------------------
         // Check if a state transition is needed
-        if (nextState[0] != "")
+        if (Q[stateQueue + 1] != "")
         {
-            if (millis() >= executeNextStateWithDelay[0])
+            if (millis() >= QDelay[stateQueue + 1])
             {
                 executeTransition();
             }
         }
-
-        // Execute the current state's function
-        if (states.count(currentState) > 0)
+        else if (states.count(Q[stateQueue]) > 0) // Execute the current state's function
         {
-            states[currentState]();
+            states[Q[stateQueue]]();
         }
         else if (errorHandler)
         {
-            handleError("State not found: " + currentState);
+            handleError("State not found: " + Q[stateQueue]);
         }
+        //----------------------------------------
     }
     else
     {
+        //----------------------------------------
         if (!suspended && logon)
         {
-            log(COLOR_ORANGE, TEXT_NORMAL, "[%s]: suspended %s\n", stateMachineName.c_str(), currentState.c_str());
+            log(COLOR_ORANGE, TEXT_NORMAL, "suspended %s\n", Q[stateQueue].c_str());
         }
         suspended = true;
+        //----------------------------------------
     }
 }
 
@@ -127,7 +127,7 @@ bool StateMachine::forcedSuspend(unsigned long timeout)
     force_suspend = true;
     while ((!suspended) && (millis() - startTime < timeout))
     {
-        delay(100);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
     return suspended;
 }
@@ -143,143 +143,105 @@ void StateMachine::resume(void)
 {
     if (force_suspend && logon)
     {
-        log(COLOR_ORANGE, TEXT_NORMAL, "[%s]: resume %s\n", stateMachineName.c_str(), currentState.c_str());
+        log(COLOR_ORANGE, TEXT_NORMAL, "resume %s\n", Q[stateQueue].c_str());
     }
     force_suspend = false;
 }
 
-void StateMachine::setNextState(const String &stateName, int execute_Next_State_With_Delay)
+void StateMachine::setIdleState(int execute_idle_state_after_delay)
+{
+    setNextState("setIdleState", execute_idle_state_after_delay);
+}
+void StateMachine::setNextState(const String &stateName, int execute_next_state_after_delay)
 {
     bool exist = false;
-    for (int i = 0; i < _STATE_BUFFER; i++)
+    for (int i = 1; i < stateQueue * 2 + 1; i++)
     {
-        if (nextState[i] == "")
+        if (Q[stateQueue + i] == "")
         {
-            executeNextStateWithDelay[i] = execute_Next_State_With_Delay + millis();
-            nextState[i] = stateName;
+            QDelay[stateQueue + i] = execute_next_state_after_delay + millis();
+            Q[stateQueue + i] = stateName;
             exist = true;
             break;
         }
     }
-    if (!exist)
+    if (!exist && logon)
     {
-        log(COLOR_RED, TEXT_NORMAL, "[%s]: Error next state queue is full for %s\n", stateMachineName.c_str(), stateName.c_str());
+        log(COLOR_RED, TEXT_NORMAL, "Error next state queue is full for %s\n", stateName.c_str());
     }
 }
 bool StateMachine::recentlyDone(const String &stateName)
 {
-    return ((nextState[0] != stateName) && (currentState != stateName) && (Q_1_State == stateName));
+    return ((Q[stateQueue + 1] != stateName) && (Q[stateQueue] != stateName) && (Q[stateQueue - 1] == stateName));
 }
-String StateMachine::getState(stateCode state)
+String StateMachine::getState(int state)
 {
-    switch (state)
+    return getQ(state);
+}
+
+    String StateMachine::getQ(int state)
     {
-    case current:
-        return currentState;
-    case next:
-        return nextState[0];
-    case Q_1:
-        return Q_1_State;
-    case Q_2:
-        return Q_2_State;
-    case Q_3:
-        return Q_3_State;
-    case Q_4:
-        return Q_4_State;
-    case Q_5:
-        return Q_5_State;
-    case Q_6:
-        return Q_6_State;
-    default:
+        int Qstate = state + stateQueue;
+        if (Qstate >= 0 && Qstate < stateQueue * 2 + 1)
+        {
+            return Q[Qstate];
+        }
+        if (logon)
+        {
+            log(COLOR_RED, TEXT_NORMAL, "Error input of getQ is not valid\n");
+        }
         return "";
     }
-}
-void StateMachine::cleanState(void)
-{
-    Q_6_State = "";
-    Q_5_State = "";
-    Q_4_State = "";
-    Q_3_State = "";
-    Q_2_State = "";
-    Q_1_State = "";
-    currentState = "";
-    ;
-    nextState[0] = "";
-}
-void StateMachine::executeTransition()
-{
-    // Call exit handler for the current state
-    if (exitHandler)
+    void StateMachine::printQ(void)
     {
-        exitHandler(currentState);
+        String str = "";
+        for (int i = 0; i < stateQueue * 2; i++)
+        {
+            if (Q[i] != "")
+            {
+                str += "Q[" + String(i - stateQueue) + "]= " + Q[i] + " ,";
+            }
+        }
+
+        if (Q[stateQueue * 2] != "")
+        {
+            str += "Q[" + String(stateQueue) + "]= " + Q[stateQueue * 2];
+        }
+        log(COLOR_LIGHT_MAGENTA, TEXT_NORMAL, "%s\n", str.c_str());
     }
-    // Perform state transition (shift states down the line)
-    Q_6_State = Q_5_State;
-    Q_5_State = Q_4_State;
-    Q_4_State = Q_3_State;
-    Q_3_State = Q_2_State;
-    Q_2_State = Q_1_State;
-    Q_1_State = currentState;
-    // Update current state
-    currentState = nextState[0];
-    for (int i = 0; i < _STATE_BUFFER - 2; i++)
+    void StateMachine::cleanState(void)
     {
-        nextState[i] = nextState[i + 1];
+        for (int i = 0; i < stateQueue * 2 + 1; i++)
+        {
+            Q[i] = "";
+        }
     }
-    nextState[_STATE_BUFFER - 1] = "";
-
-    // Call enter handler for the new state
-    if (enterHandler)
+    void StateMachine::executeTransition()
     {
-        enterHandler(currentState);
-    }
-}
+        // Call exit handler for the current state
+        if (exitHandler)
+        {
+            exitHandler(Q[stateQueue]);
+        }
+        // Perform state transition (shift states down the line)
+        for (int i = 0; i < stateQueue * 2; i++)
+        {
+            Q[i] = Q[i + 1];
+            QDelay[i] = QDelay[i + 1];
+        }
+        Q[stateQueue * 2] = "";
+        QDelay[stateQueue * 2] = 0;
 
-void StateMachine::handleError(const String &errorMessage)
-{
-    if (errorHandler)
+        // Call enter handler for the new state
+        if (enterHandler)
+        {
+            enterHandler(Q[stateQueue]);
+        }
+    }
+    void StateMachine::handleError(const String &errorMessage)
     {
-        errorHandler(errorMessage);
+        if (errorHandler)
+        {
+            errorHandler(errorMessage);
+        }
     }
-}
-//============================================================================================================================================
-// // Function to send a report for a given state
-// void StateMachine::sendReport(const String &stateName, const JsonObject &report)
-// {
-//     // Allocate a new DynamicJsonDocument for this state
-//     stateReports[stateName] = DynamicJsonDocument(1024);
-//     // Copy the contents of the report to the new document
-//     stateReports[stateName].set(report);
-// }
-
-// //============================================================================================================================================
-// // Function to wait for a report for a given state, with a timeout
-// bool StateMachine::waitForReport(const String &stateName, JsonObject &report, int timeoutMs)
-// {
-//     int timeElapsed = 0;
-//     const int pollingInterval = 100; // Poll every 100ms
-//     // Continue polling until the report is available or timeout occurs
-//     while (timeElapsed < timeoutMs)
-//     {
-//         // Check if the report for the given state is available
-//         auto it = stateReports.find(stateName);
-//         if (it != stateReports.end())
-//         {
-//             // Get the JsonObject from the stored DynamicJsonDocument
-//             report = it->second.as<JsonObject>();
-
-//             // Optionally, remove the report after retrieval
-//             stateReports.erase(it);
-
-//             return true;
-//         }
-//         // Delay to avoid busy-waiting (FreeRTOS delay)
-//         vTaskDelay(pdMS_TO_TICKS(pollingInterval));
-//         // Increment the elapsed time
-//         timeElapsed += pollingInterval;
-//     }
-//     // If no report was received within the timeout, return an empty report
-//     DynamicJsonDocument errorReport(256);  // Create a document for the error case
-//     report = errorReport.to<JsonObject>(); // Return an empty object
-//     return false;                          // Indicate timeout
-// }
--- PAT_OperatingSystem/src/PAT_StateMachine.h
+++ PAT_OperatingSystem/src/PAT_StateMachine.h
@@ -8,34 +8,23 @@
 #include <string>
 #include <ArduinoJson.h>
 #include "PAT_Debug.h"
-#define _STATE_BUFFER 20
+#define stateQueue 10
 
-enum stateCode
-{
-    current = 0,
-    next,
-    Q_1,
-    Q_2,
-    Q_3,
-    Q_4,
-    Q_5,
-    Q_6,
-};
 class StateMachine : public Class_Log
 {
 private:
-    // std::map<String, DynamicJsonDocument> stateReports;
     using StateFunction = std::function<void()>;
-    using StateTransitionHandler = std::function<void(const String &)>;
-    using ErrorHandler = std::function<void(const String &)>;
+    using eventFunction = std::function<void(const String &)>;
+    using statesFunction = std::map<String, StateFunction>;
 
-    String nextState[_STATE_BUFFER], currentState, Q_1_State, Q_2_State, Q_3_State, Q_4_State, Q_5_State, Q_6_State, stateMachineName; // name os state to transition
-    std::map<String, StateFunction> states;                                                                                            // Map of state names to functions
-    StateTransitionHandler enterHandler;                                                                                               // Handler for entering a state
-    StateTransitionHandler exitHandler;                                                                                                // Handler for exiting a state
-    ErrorHandler errorHandler;                                                                                                         // Handler for errors
+    statesFunction states;
+    eventFunction enterHandler, exitHandler, errorHandler;
+
+    uint32_t QDelay[stateQueue * 2 + 1];
+    String Q[stateQueue * 2 + 1]; 
+    
     bool suspended, force_suspend, logon;
-    unsigned long executeNextStateWithDelay[_STATE_BUFFER];
+
     // Execute state transitions if needed
     void executeTransition();
     void handleError(const String &errorMessage);
@@ -50,11 +39,11 @@ public:
     void addState(const String &stateName, StateFunction stateFunc);
 
     // Register transition handlers for entering and exiting states
-    void onEnterState(StateTransitionHandler handler);
-    void onExitState(StateTransitionHandler handler);
+    void onEnterState(eventFunction handler);
+    void onExitState(eventFunction handler);
 
     // Register an error handler
-    void setErrorHandler(ErrorHandler handler);
+    void setErrorHandler(eventFunction handler);
 
     // Update method to execute the current state logic
     void update();
@@ -63,12 +52,14 @@ public:
     bool isSuspended(void);
     void suspend(void);
     void resume(void); // Set the next state to transition to
-    void setNextState(const String &stateName, int execute_Next_State_With_Delay = 0);
+    void setNextState(const String &stateName, int execute_next_state_after_delay = 0);
+    void setIdleState(int execute_idle_state_after_delay = 0);
     void cleanState(void);
-    String getState(stateCode state);
+    String getQ(int state);
+    String getState(int state);
     void logOn(String stateMachine);
-    void sendReport(const String &stateName, const JsonObject &report);
-    bool waitForReport(const String &stateName, JsonObject &report, int timeoutMs);
+    void logOff(void);
+    void printQ(void);
 };
 
 #endif // PAT_STATE_MACHINE_H
